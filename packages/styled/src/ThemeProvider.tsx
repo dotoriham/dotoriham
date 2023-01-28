5e795a21724ec819df1ad5c2de36d78c3843f5dc
ksmfou98 <ksmfou98@naver.com>
2023-01-28 18:42:17 +0900

    feat: workspace 주석 제거

--- packages/styled/src/ThemeProvider.tsx
+++ packages/styled/src/ThemeProvider.tsx
@@ -1,7 +1,5 @@
 import { PropsWithChildren } from "react";
-
 import { ThemeProvider as _ThemeProvider } from "styled-components";
-
 import { GlobalStyle } from "./globalStyles";
 import { lightTheme } from "./theme";
 

752ce453d5160baece391a33a0f1c6c905d0091d
dohyeonlee <dohyeonlee@toss.im>
2024-01-14 01:31:23 +0900

    feat: tsconfig 패키지 기본 옵션 수정

--- packages/ui/types/index.d.ts
+++ packages/ui/types/index.d.ts
@@ -1,13 +1,3 @@
 import '@testing-library/jest-dom';
 
-declare module '*.module.css' {
-  const value: Record<string, string>;
-  export default value;
-}
-
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
 export {};

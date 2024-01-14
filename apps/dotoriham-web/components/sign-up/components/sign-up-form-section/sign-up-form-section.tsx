85d111605232389f0ff8f7cee225091774e2bcea
dohyeonlee <dohyeonlee@toss.im>
2024-01-14 19:02:40 +0900

    feat: dotoriham input에 eye icon 추가

--- apps/dotoriham-web/components/sign-up/components/sign-up-form-section/sign-up-form-section.tsx
+++ apps/dotoriham-web/components/sign-up/components/sign-up-form-section/sign-up-form-section.tsx
@@ -1,9 +1,15 @@
+import { EyeInactiveIcon } from '@dotoriham/icons';
 import { Input } from '@dotoriham/ui';
 
 export const SignUpFormSection = () => {
   return (
     <form>
-      <Input size="lg" mb={20} placeholder="이메일" />
+      <Input
+        size="lg"
+        mb={20}
+        placeholder="이메일"
+        rightComponent={<EyeInactiveIcon fontSize={20} />}
+      />
       <Input size="lg" mb={20} placeholder="비밀번호" />
     </form>
   );

7ec2880b05bb22661e2f3ad2a481fb6211c0c524
ksmfou98 <ksmfou98@naver.com>
2024-01-22 23:51:38 +0900

    feat: sign-up 링크 버튼 구현

--- apps/dotoriham-web/components/sign-up/sign-up-linked-button/sign-up-linked-button.tsx
+++ apps/dotoriham-web/components/sign-up/sign-up-linked-button/sign-up-linked-button.tsx
@@ -1,3 +1,25 @@
+import { Flex, Typography, fixedColor } from '@dotoriham/ui';
+import Link from 'next/link';
+
+import { ROUTES } from '../../../constants/routes';
+
 export const SignUpLinkedButton = () => {
-  return <div>Linked</div>;
+  return (
+    <Flex align="center">
+      <Typography type="system_14_400">이미 회원이신가요?</Typography>
+      <Typography
+        as={Link}
+        href={ROUTES.signin({
+          referrer: 'sign-up',
+        })}
+        type="system_14_500"
+        style={{
+          textDecoration: 'underline',
+        }}
+        ml={12}
+        color={fixedColor.green9}>
+        로그인
+      </Typography>
+    </Flex>
+  );
 };

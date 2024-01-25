747c9de7dc3686564515a5778b02e692758e65bd
dohyeonlee <dohyeonlee@toss.im>
2024-01-25 13:54:59 +0900

    feat: useMounted hooks 구현

--- packages/hooks/src/use-mounted/use-mounted.ts
+++ packages/hooks/src/use-mounted/use-mounted.ts
@@ -1,1 +1,11 @@
-export const useMounted = () => {};
+import { useEffect, useState } from 'react';
+
+export const useMounted = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted;
+};

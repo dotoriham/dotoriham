3f7a3830033223d36107a6fa5bab7973e6cb1594
dohyeonlee <dohyeonlee@toss.im>
2024-01-25 13:49:32 +0900

    feat: use-toggle hook 구현

--- packages/hooks/src/use-toggle/use-toggle.ts
+++ packages/hooks/src/use-toggle/use-toggle.ts
@@ -1,1 +1,11 @@
-export const useToggle = () => {};
+import { useCallback, useState } from 'react';
+
+export const useToggle = (initialValue = false) => {
+  const [value, setValue] = useState(initialValue);
+
+  const toggleValue = useCallback(() => {
+    setValue((value) => !value);
+  }, []);
+
+  return [value, toggleValue, setValue] as const;
+};

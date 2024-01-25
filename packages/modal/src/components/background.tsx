c684d328f4221f72bf98447c9a0f3792cd6f7c58
dohyeonlee <dohyeonlee@toss.im>
2024-01-25 19:34:03 +0900

    feat: 컴포넌트가 종료될 때도 애니메이션을 주도록 구현

--- packages/modal/src/components/background.tsx
+++ packages/modal/src/components/background.tsx
@@ -1,4 +1,4 @@
-import { motion } from '@dotoriham/animate';
+import { motion, AnimatePresence } from '@dotoriham/animate';
 
 interface BackgroundProps {
   isOpen: boolean;
@@ -6,25 +6,25 @@ interface BackgroundProps {
 }
 
 export const Background = ({ isOpen, onClick }: BackgroundProps) => {
-  if (isOpen === false) {
-    return null;
-  }
-
   return (
-    <motion.div
-      onClick={onClick}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 0.6 }}
-      exit={{ opacity: 0 }}
-      style={{
-        backgroundColor: 'black',
-        height: '100%',
-        left: 0,
-        position: 'fixed',
-        top: 0,
-        width: '100%',
-        zIndex: 10,
-      }}
-    />
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          onClick={onClick}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.6 }}
+          exit={{ opacity: 0 }}
+          style={{
+            backgroundColor: 'black',
+            height: '100%',
+            left: 0,
+            position: 'fixed',
+            top: 0,
+            width: '100%',
+            zIndex: 10,
+          }}
+        />
+      )}
+    </AnimatePresence>
   );
 };

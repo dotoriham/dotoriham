ab131dfcf2a60d3e39ea490f264ea135de88a6ec
dohyeonlee <dohyeonlee@toss.im>
2024-01-29 19:30:00 +0900

    feat(modal): modal 애니메이션은 center 기준으로 되게 설정

--- packages/modal/src/components/modal-container.tsx
+++ packages/modal/src/components/modal-container.tsx
@@ -2,42 +2,39 @@ import { ReactNode } from 'react';
 
 import { AnimatePresence, motion } from '@dotoriham/animate';
 
+import classes from './modal.module.css';
+
 interface ModalContainerProps {
   children: ReactNode;
   isOpen: boolean;
 }
-
-const slideUpToCenter = {
-  exit: {
-    opacity: 0,
-    y: '100vh',
-  },
-  hidden: {
-    opacity: 0,
-    y: '100vh',
-  },
-  visible: {
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-      type: 'spring',
-    },
-    y: '40vh',
-  },
-};
 
 export const ModalContainer = ({ children, isOpen }: ModalContainerProps) => {
   return (
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          variants={slideUpToCenter}
+          variants={{
+            exit: {
+              opacity: 0,
+              y: '100vh',
+            },
+            hidden: {
+              opacity: 0,
+              y: '100vh',
+            },
+            visible: {
+              opacity: 1,
+              transition: {
+                duration: 0.5,
+                type: 'spring',
+              },
+              y: '40vh',
+            },
+          }}
           initial="hidden"
           animate="visible"
-          style={{
-            position: 'absolute',
-            zIndex: 50, // TODO z-index를 상수로 관리 + 해당 코드를 module css로 분리
-          }}
+          className={classes.root}
           exit="exit">
           {children}
         </motion.div>

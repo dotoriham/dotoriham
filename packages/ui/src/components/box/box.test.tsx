db9af2e65b84d28d2992a190f45de7264d88a9e0
dohyeonlee <dohyeonlee@toss.im>
2024-02-01 13:52:59 +0900

    test(ui): Box 컴포넌트 테스트 코드 작성

--- packages/ui/src/components/box/box.test.tsx
+++ packages/ui/src/components/box/box.test.tsx
@@ -1,15 +1,38 @@
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Box } from './box';
 
 describe('Box', () => {
-  it('should render', () => {
+  afterEach(() => {
+    screen.debug();
+  });
+  test('should render', () => {
     render(<Box>hihi</Box>);
   });
 
-  it("Box의 className이 'box'인지 확인", () => {
+  test('Box의 className를 지정할 수 있다.', () => {
     const { container } = render(<Box className="box">hihi</Box>);
     expect(container.firstChild).toHaveClass('box');
-    screen.debug();
+  });
+
+  test('Box의 Element를 변경할 수 있다 (다형성)', () => {
+    const { container } = render(<Box as="span">hihi</Box>);
+    expect(container.firstChild).toBeInstanceOf(HTMLSpanElement);
+  });
+
+  test('Box의 style을 지정할 수 있다', () => {
+    const { container } = render(<Box style={{ color: 'red' }}>hihi</Box>);
+    expect(container.firstChild).toHaveStyle('color: red');
+  });
+
+  test('Box의 systemStyle을 지정할 수 있다', () => {
+    const { container } = render(
+      <Box m={1} p={1} bg="red">
+        hihi
+      </Box>,
+    );
+    expect(container.firstChild).toHaveStyle('margin: 1px');
+    expect(container.firstChild).toHaveStyle('padding: 1px');
+    expect(container.firstChild).toHaveStyle('background-color: red');
   });
 });

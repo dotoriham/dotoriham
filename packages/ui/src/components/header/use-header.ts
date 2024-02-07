36370bda081edf08f99ed71f878b5adc45b4eda7
dohyeonlee <dohyeonlee@toss.im>
2024-02-07 13:05:18 +0900

    feat(ui): header 제거

--- packages/ui/src/components/header/use-header.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, useStore } from '@dotoriham/store';
-
-const headerStore = createStore<string>('null');
-
-export const useHeader = () => {
-  const [state, setState] = useStore(headerStore);
-
-  return {};
-};

ece88b8218ed0d05b405ee1ecf81dc5c06634085
dohyeonlee <dohyeonlee@toss.im>
2024-02-08 13:41:56 +0900

    feat(web): 사이드바, 헤더, 푸터 레이아웃 설정

--- apps/dotoriham-web/app/home/layout.tsx
+++ apps/dotoriham-web/app/home/layout.tsx
@@ -1,8 +1,51 @@
+import { Sidebar } from '../../components/@shared/sidebar/sidebar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      Layout
-      {children}
-    </div>
+    <>
+      <header
+        style={{
+          backgroundColor: 'lightblue',
+          height: '48px',
+          margin: '0 auto',
+          padding: '10px',
+          width: '1440px',
+        }}>
+        헤더영역
+      </header>
+
+      <main
+        style={{
+          margin: '0 auto',
+          minHeight: 'calc(100vh - 48px - 56px)',
+          width: '1440px',
+        }}>
+        <div
+          style={{
+            display: 'flex',
+            height: 'calc(100vh - 48px - 56px)',
+          }}>
+          <Sidebar />
+          <div
+            style={{
+              backgroundColor: 'lightyellow',
+              flex: '1 0 auto',
+            }}>
+            {children}
+          </div>
+        </div>
+      </main>
+
+      <footer
+        style={{
+          backgroundColor: 'lightgreen',
+          height: '56px',
+          margin: '0 auto',
+          padding: '10px',
+          width: '1440px',
+        }}>
+        푸터영역
+      </footer>
+    </>
   );
 }
--- apps/dotoriham-web/components/@shared/sidebar/sidebar.tsx
+++ apps/dotoriham-web/components/@shared/sidebar/sidebar.tsx
@@ -1,3 +1,41 @@
+'use client';
+
+import { getClassNames } from '@dotoriham/ui';
+
+import classes from './sidebar.module.css';
+const cx = getClassNames(classes);
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className={cx('root')}>
+      Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar
+      영역입니다Sidebar 영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar
+      영역입니다Sidebar 영역입니다 Sidebar 영역입니다Sidebar 영역입니다Sidebar
+      영역입니다
+    </aside>
+  );
 };

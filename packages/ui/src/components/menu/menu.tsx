9d78f97a1bb52ec651f8f7c85c21b356971d3925
dohyeonlee <dohyeonlee@toss.im>
2024-03-05 14:01:57 +0900

    feat(ui): menu 하위 컴포넌트 생성

--- packages/ui/src/components/menu/menu.tsx
+++ packages/ui/src/components/menu/menu.tsx
@@ -1,9 +1,25 @@
-export const Menu = () => {
-  return (
-    <div>
-      Menu
-    </div>
-  );
+import { ReactNode } from 'react';
+
+interface MenuProps {
+  children: ReactNode;
+}
+
+export const Menu = ({ children }: MenuProps) => {
+  return <div>{children}</div>;
 };
 
+const Target = () => {
+  return <div>Target</div>;
+};
 
+const DropDown = () => {
+  return <div>Dropdown</div>;
+};
+
+const Label = () => {
+  return <div>Label</div>;
+};
+
+const Item = () => {
+  return <div>Item</div>;
+};

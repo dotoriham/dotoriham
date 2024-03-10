1a365307feb9b3e2fb12fc0656048c2dcb04b18e
dohyeonlee <dohyeonlee@toss.im>
2024-03-10 18:39:23 +0900

    feat(react-utils): create safe context util 함수 구현

---
+++ packages/react-utils/src/create-safe-context/create-safe-context.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, createContext, useContext } from 'react';
+
+export function CreateSafeContext<ContextValue>(errorMessage?: string) {
+  const Context = createContext<ContextValue | null>(null);
+
+  const useSafeContext = () => {
+    const context = useContext(Context);
+
+    if (context == null) {
+      throw new Error(
+        errorMessage || 'useContext must be inside a Provider with a value',
+      );
+    }
+
+    return context;
+  };
+
+  const Provider = ({
+    children,
+    value,
+  }: {
+    children: ReactNode;
+    value: ContextValue;
+  }) => {
+    return <Context.Provider value={value}>{children}</Context.Provider>;
+  };
+
+  return [Provider, useSafeContext] as const;
+}

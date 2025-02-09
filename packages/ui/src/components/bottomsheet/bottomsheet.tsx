f7a267c27d9db1364d892f54a98bec963c0dc2b0
ksmfou98 <ksmfou98@naver.com>
2025-02-09 22:36:11 +0900

    fix: bottomsheet css

--- packages/ui/src/components/bottomsheet/bottomsheet.tsx
+++ packages/ui/src/components/bottomsheet/bottomsheet.tsx
@@ -1,9 +1,8 @@
+import classes from './bottomsheet.module.css';
+import { getClassNames } from '../../utils';
+
+const cx = getClassNames(classes);
+
 export const Bottomsheet = () => {
-  return (
-    <div>
-      Bottomsheet
-    </div>
-  );
+  return <div className={cx('root')}>Bottomsheet</div>;
 };
-
-

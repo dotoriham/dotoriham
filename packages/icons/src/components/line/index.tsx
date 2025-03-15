8bc4af242371a802168a0dec16613dc1a7f2fb6e
ksmfou98 <ksmfou98@naver.com>
2025-03-15 23:51:24 +0900

    feat: etc icon 추가

--- packages/icons/src/components/line/index.tsx
+++ packages/icons/src/components/line/index.tsx
@@ -1,5 +1,6 @@
 export { default as ArrowIcon } from './Arrow';
 export { default as BellIcon } from './Bell';
+export { default as EtcIcon } from './Etc';
 export { default as EyeActiveIcon } from './EyeActive';
 export { default as EyeInactiveIcon } from './EyeInactive';
 export { default as PlusIcon } from './Plus';
